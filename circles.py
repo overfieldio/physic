177385a401f1062601a81ca49c5bf83b047a2f9c
Overfieldio <overfield978@gmail.com>
2020-02-03 22:45:50 +0000

    +adds birth and death functionality, circles can die or produce offspring per tick

--- circles.py
+++ circles.py
@@ -17,10 +17,17 @@ running = True
 console = False
 
 circle_radius = 5
-max_clicks = 10
 max_circles = 10
+
 circles = []
 
+circle_deathrate = 0.005
+circle_birthrate = 0.005
+
+deaths = 0
+
+for i in range(0, max_circles):
+    circles.append((random.randint(0, screen_width), random.randint(0, screen_height)))
 
 pygame.display.init()
 while running:
@@ -41,6 +48,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_c:
                 circles = []
+                corpses = []
             if event.key == pygame.K_BACKQUOTE:
                 if not console:
                     console = True
@@ -55,24 +63,35 @@ while running:
         textsurface = font.render('Circles : ' + str(circles), False, (255, 255, 255))
         screen.blit(textsurface, (0, 50))
 
-
-    #slightly changes the location of each circle
+    #cycles through each circle checking (death -> birth -> move)
     circles_perturbed = []
     for i in circles:
-        x = i[0] + random.randint(-1,1)
-        y = i[1] + random.randint(-1,1)
-        if x < 0:
-            x = 0
-        elif x > screen_width:
-            x = screen_width
-        if y < 0:
-            y = 0
-        elif y > screen_height:
-            y = screen_height
-        circles_perturbed.append((x, y))
-        circles = circles_perturbed
+        x = i[0]
+        y = i[1]
+        # does the circle die
+        if random.random() < circle_deathrate:
+            deaths += 1
+        else:
+            # produces offspring near the parent
+            if random.random() < circle_birthrate:
+                circles_perturbed.append((x + random.randint(-1,1), y + random.randint(-1,1)))
+            # moves the circle in a random direction
+            x += random.randint(-1,1)
+            y += random.randint(-1,1)
+            if x < 0:
+                x = 0
+            elif x > screen_width:
+                x = screen_width
+            if y < 0:
+                y = 0
+            elif y > screen_height:
+                y = screen_height
+            circles_perturbed.append((x, y))
+            pygame.draw.circle(screen, (255, 255, 255), (x, y), circle_radius)
+    circles = circles_perturbed
 
-        pygame.draw.circle(screen, (255, 255, 255), (x, y), circle_radius)
+    textsurface = font.render('Alive: ' + str(len(circles)) + ', Dead: ' + str(deaths), False, (255, 255, 255))
+    screen.blit(textsurface, (0, screen_height - 70))
 
     textsurface = font.render('` : console', False, (255, 255, 255))
     screen.blit(textsurface, (0, screen_height - 50))
