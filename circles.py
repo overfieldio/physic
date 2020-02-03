fb95b48895b944ded6ee8336c5cacd5d6b1c2bcb
Overfieldio <overfield978@gmail.com>
2020-02-03 20:40:49 +0000

    Slightly 'wiggles' each circle per tick

--- circles.py
+++ circles.py
@@ -1,7 +1,11 @@
 import pygame
+import random
+import math
 
 pygame.init()
+clock = pygame.time.Clock()
 
+PROGRAM_FPS = 30
 screen_width = 500
 screen_height = 400
 
@@ -13,8 +17,9 @@ running = True
 console = False
 
 circle_radius = 5
-max_clicks = 4
-clicks = []
+max_clicks = 10
+max_circles = 10
+circles = []
 
 
 pygame.display.init()
@@ -27,15 +32,15 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            if len(clicks) == max_clicks:
-                clicks = clicks[1:]
-                clicks.append(pos)
+            if len(circles) == max_circles:
+                circles = circles[1:]
+                circles.append(pos)
             else:
-                clicks.append(pos)
+                circles.append(pos)
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_c:
-                clicks = []
+                circles = []
             if event.key == pygame.K_BACKQUOTE:
                 if not console:
                     console = True
@@ -47,22 +52,37 @@ while running:
         textsurface = font.render('Last clicked : ' + str(pos), False, (255, 255, 255))
         screen.blit(textsurface, (0, 0))
 
-        textsurface = font.render('Clicks : ' + str(clicks), False, (255, 255, 255))
+        textsurface = font.render('Circles : ' + str(circles), False, (255, 255, 255))
         screen.blit(textsurface, (0, 50))
 
-    for i in clicks:
-        pygame.draw.circle(screen, (255, 255, 255), i, circle_radius)
 
-    if len(clicks) >= 2:
-        pygame.draw.lines(screen, (255, 255, 255), True, clicks)
+    #slightly changes the location of each circle
+    circles_perturbed = []
+    for i in circles:
+        x = i[0] + random.randint(-1,1)
+        y = i[1] + random.randint(-1,1)
+        if x < 0:
+            x = 0
+        elif x > screen_width:
+            x = screen_width
+        if y < 0:
+            y = 0
+        elif y > screen_height:
+            y = screen_height
+        circles_perturbed.append((x, y))
+        circles = circles_perturbed
+
+        pygame.draw.circle(screen, (255, 255, 255), (x, y), circle_radius)
 
     textsurface = font.render('` : console', False, (255, 255, 255))
     screen.blit(textsurface, (0, screen_height - 50))
 
-    textsurface = font.render('C : clears all squares', False, (255, 255, 255))
+    textsurface = font.render('C : clears all circles', False, (255, 255, 255))
     screen.blit(textsurface, (0, screen_height - 30))
 
     pygame.display.flip()
+
+    clock.tick(PROGRAM_FPS)
 pygame.quit()
 
 
